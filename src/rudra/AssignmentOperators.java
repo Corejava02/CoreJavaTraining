70fd390cbaefd065cd982a1998e9c568987b5360
Corejava02 <137301305+Corejava02@users.noreply.github.com>
2023-06-30 21:03:33 +0530

    odd or even.java

--- src/rudra/AssignmentOperators.java
+++ src/rudra/AssignmentOperators.java
@@ -1,22 +1,10 @@
-package Date29_6_2023;
+public class EvenOrOdd {
+    public static void main(String[]args) {
+        int number = 1000;
 
-/**
- * used to assign values into variables
- */
-
-public class AssignmentOperators {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int result = a+b;
-        System.out.println(result);
-
-        int num = 7;
-        int c = num++;
-        System.out.println(num);
-        System.out.println(c);
-
-        int c1 = num++;
-        System.out.println(c1);
+        if(number % 2 == 0)
+        System.out.println(number +"is even");
+        else
+        System.out.println(number +"is odd");
     }
 }
