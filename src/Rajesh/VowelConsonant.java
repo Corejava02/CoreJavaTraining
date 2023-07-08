be196c5e6ca5e5dc8a423221795ab011b27b56f3
devraajdeveloper <31042772+devraajdeveloper@users.noreply.github.com>
2023-07-08 08:54:51 +0530

    Add files via upload

---
+++ src/Rajesh/VowelConsonant.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character:");
+        char c = sc.next().charAt(0);
+        switch (c) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("It is a consonant.");
+                break;
+        }
+
+
+    }
+}
